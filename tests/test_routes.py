7b9f53d16255252d9588562179ec634a40fc63fa
dsalerno76 <dsalerno76@gmail.com>
2024-07-27 06:58:27 -0400

    test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -178,3 +178,11 @@ class TestProductRoutes(TestCase):
         data = response.get_json()
         # logging.debug("data = %s", data)
         return len(data)
+
+    def test_get_product(self):
+        """ Test reading a product """
+        test_product = _create_products(1)[0]
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], test_product.name)
